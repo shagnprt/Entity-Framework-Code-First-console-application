912f463d0ca087fa5190948f45e12fb82f12061a
shagnprt <shagun00019@gmail.com>
2024-10-22 18:47:40 -0300

    Add files via upload

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTime3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "shagun" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+
+                Console.Write("Student saved successfully!");
+                Console.ReadLine();
+            }
+        }
+    }
+}
